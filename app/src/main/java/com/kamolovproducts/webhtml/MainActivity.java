0df505ea4dc41cfbefa0cbb93b0dfe13a8d1ee4f
Foteh Kamolov <55693215+fotehkamolov96@users.noreply.github.com>
2022-01-10 03:24:13 +0300

    Initial commit

--- app/src/main/java/com/kamolovproducts/webhtml/MainActivity.java
+++ app/src/main/java/com/kamolovproducts/webhtml/MainActivity.java
@@ -1,80 +1,96 @@
 package com.kamolovproducts.webhtml;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.MailTo;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
-import android.webkit.URLUtil;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView mWebView;
+    WebView mWebView;
+    ProgressBar progressBar;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = findViewById(R.id.webView);
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
-        // Enable Javascript
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-
         // Увличение контента
-       /* mWebView.setInitialScale(2);
+        /* mWebView.setInitialScale(2);
         mWebView.getSettings().setBuiltInZoomControls(true);
         mWebView.getSettings().setUseWideViewPort(true);*/
 
+        progressBar = findViewById(R.id.progressBar);
+        mWebView = findViewById(R.id.webView);
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl("file:///android_asset/index.html");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new myWebClient());
 
 
-        mWebView.setWebViewClient(new WebViewClient() {
-            String currentUrl;
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                currentUrl = url;
-                Uri uri = Uri.parse("https://www.instagram.com/omuzishinamoz/");
-                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
-                likeIng.setPackage("com.instagram.android");
-
-                try {
-                    startActivity(likeIng);
-                } catch (ActivityNotFoundException e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("https://www.instagram.com/omuzishinamoz/")));
-                }
-                return false;
-            }
-
-
-        });
-
-
+        /*
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        */
     }
+
+    public class myWebClient extends WebViewClient {
+        String currentUrl;
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progressBar.setVisibility(View.GONE);
+            view.setVisibility(mWebView.VISIBLE);
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            currentUrl = url;
+
+            Uri uri = Uri.parse("https://www.instagram.com/omuzishinamoz/");
+            Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+            likeIng.setPackage("com.instagram.android");
+
+            try {
+                startActivity(likeIng);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.instagram.com/omuzishinamoz/")));
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -85,3 +101,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+
+
+
+
+
+/*    public class myWebClient extends WebViewClient{
+        String currentUrl;
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            currentUrl = url;
+
+            Uri uri = Uri.parse("https://www.instagram.com/omuzishinamoz/");
+            Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+            likeIng.setPackage("com.instagram.android");
+
+            try {
+                startActivity(likeIng);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.instagram.com/omuzishinamoz/")));
+            }
+            return false;
+
+        }
+
+
+    }*/
